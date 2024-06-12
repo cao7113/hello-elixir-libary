d365947ae4e1b0f42498dcfacdf1dc8e46ab7df0
Ryn Cao <cao7113@gmail.com>
2024-06-12 16:17:14 +0800

    fix source url

--- mix.exs
+++ mix.exs
@@ -8,7 +8,10 @@ defmodule Hello.MixProject do
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      name: "HelloLibary",
       description: "hello elixir libary",
+      source_url: "https://github.com/cao7113/hello-libary",
+      homepage_url: "https://github.com/cao7113/hello-libary",
       package: package()
     ]
   end
@@ -24,22 +27,22 @@ defmodule Hello.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.11", only: :dev}
+      # https://hexdocs.pm/ex_doc/readme.html
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
     ]
   end
 
   # hex package metadata as https://hex.pm/docs/publish
   def package do
     [
-      # must
+      # This option is only needed when you don't want to use the OTP application name
+      # name: "hello_libary",
       licenses: ["Apache-2.0"],
       maintainers: ["cao7113"],
       links: %{
         "GitHub" => "https://github.com/cao7113/hello-libary",
         "Docs" => "https://hexdocs.pm/hello_libary/"
       }
-      # optional
-      # files: []
     ]
   end
 end
